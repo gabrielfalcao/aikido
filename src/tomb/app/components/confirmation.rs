06956d0738c6fac14fc3f7b867e2de84549086bb
Gabriel Falcão (he/him/his) <gabriel.falcao@nytimes.com>
2021-12-24 23:20:31 +0100

    introduce base widget for confirmation

---
+++ src/tomb/app/components/confirmation.rs
@@ -0,0 +1,126 @@
+use crate::ironpunk::LoopEvent::*;
+use crate::ironpunk::*;
+
+#[allow(unused_imports)]
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use std::io;
+#[allow(unused_imports)]
+use tui::{
+    backend::CrosstermBackend,
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
+    widgets::{Block, BorderType, Borders, Paragraph, Wrap},
+    Frame, Terminal,
+};
+
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub struct Confirmation {
+    pub title: String,
+    pub text: String,
+    active: bool,
+}
+/// Confirmation with editable content
+impl Confirmation {
+    #[allow(dead_code)]
+    pub fn new(title: &str, text: &str) -> Confirmation {
+        Confirmation {
+            title: String::from(title),
+            text: String::from(text),
+            active: true,
+        }
+    }
+    #[allow(dead_code)]
+    pub fn set_title(&mut self, title: &str) {
+        self.title = String::from(title);
+    }
+    #[allow(dead_code)]
+    pub fn set_text(&mut self, text: &str) {
+        self.text = String::from(text);
+    }
+    #[allow(dead_code)]
+    pub fn write(&mut self, c: char) {
+        self.text.push(c);
+    }
+    #[allow(dead_code)]
+    pub fn backspace(&mut self) {
+        self.text.pop();
+    }
+
+    pub fn is_active(&self) -> bool {
+        self.active
+    }
+    pub fn deactivate(&mut self) {
+        self.active = false;
+    }
+    pub fn render_in_parent(
+        &self,
+        parent: &mut Frame<CrosstermBackend<io::Stdout>>,
+        chunk: Rect,
+    ) -> Result<(), Error> {
+        let chunk = get_modal_rect(chunk);
+        let confirmation = Block::default()
+            .borders(Borders::ALL)
+            .style(
+                Style::default()
+                    .bg(Color::DarkGray)
+                    .bg(Color::White)
+                    .fg(Color::Black),
+            )
+            .title(self.title.clone())
+            .border_type(BorderType::Rounded);
+
+        let text = vec![Spans::from(Span::styled(
+            self.text.clone(),
+            Style::default()
+                .fg(Color::DarkGray)
+                .add_modifier(Modifier::BOLD),
+        ))];
+        let paragraph = Paragraph::new(text)
+            .block(confirmation)
+            .style(Style::default().bg(Color::White).fg(Color::Black))
+            .alignment(Alignment::Left)
+            .wrap(Wrap { trim: true });
+
+        parent.render_widget(paragraph, chunk);
+
+        Ok(())
+    }
+}
+
+impl Component for Confirmation {
+    fn name(&self) -> &str {
+        "Confirmation"
+    }
+    fn id(&self) -> String {
+        String::from("Confirmation")
+    }
+
+    #[allow(unused_variables)]
+    fn process_keyboard(
+        &mut self,
+        terminal: &mut Terminal<CrosstermBackend<io::Stdout>>,
+        event: KeyEvent,
+    ) -> Result<LoopEvent, Error> {
+        match event.code {
+            KeyCode::Backspace => {
+                self.backspace();
+                Ok(Propagate)
+            }
+            KeyCode::Esc => {
+                self.deactivate();
+                return Ok(Propagate);
+            }
+            KeyCode::Enter => {
+                self.write('\n');
+                return Ok(Propagate);
+            }
+            KeyCode::Char(c) => {
+                self.write(c);
+                Ok(Refresh)
+            }
+            _ => Ok(Propagate),
+        }
+    }
+}
--- src/tomb/app/components/mod.rs
+++ src/tomb/app/components/mod.rs
@@ -1,2 +1,7 @@
-pub mod modal;
+pub mod confirmation;
 pub mod menu;
+pub mod modal;
+
+pub use confirmation::*;
+pub use menu::*;
+pub use modal::*;
