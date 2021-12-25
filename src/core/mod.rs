7e48d975e4de88dfb939056c85ecb0a805abd599
Gabriel Falcão <gabriel@nacaolivre.org>
2021-12-25 17:31:26 +0100

    1.9.0

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,2 +1,2 @@
-pub const VERSION: &'static str = "1.8.0";
-pub const AUTHOR: &'static str = "Gabriel Falcão <gabriel@nacaolivre.org>";
+pub const VERSION: &'static str = "1.9.0";
+pub const AUTHOR: &'static str = "Gabriel Falcão - @gabrielfalcao";
